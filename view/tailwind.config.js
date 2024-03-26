1252bb055c50b8ff6f06dc30b8fa6af412c0d2e8
Carlos Ramírez <cadavid4003@gmail.com>
2024-03-25 21:55:05 -0500

    Arregla sobreescritura de estilos de tailwind

--- view/tailwind.config.js
+++ view/tailwind.config.js
@@ -2,15 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      main: "#F1FFF2",
-      primary: "#80ED99",
-      secondary: "#6FC2BD",
-      black: "#0E1918",
-    },
-    fontFamily: {
-      sans: ["Graphik", "sans-serif"],
-      serif: ["Merriweather", "serif"],
+    extend: {
+      colors: {
+        main: "#F1FFF2",
+        primary: "#80ED99",
+        secondary: "#6FC2BD",
+        black: "#0E1918",
+      },
+      fontFamily: {
+        sans: ["Graphik", "sans-serif"],
+        serif: ["Merriweather", "serif"],
+      },
     },
   },
   plugins: [],
