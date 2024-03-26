8e5970fd12a93e1521194b66f1b8e930ce81521d
Carlos Ramírez <cadavid4003@gmail.com>
2024-03-25 23:51:02 -0500

    Añade componente botón básico

--- view/src/App.jsx
+++ view/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
+import Button from "./components/Button/Button";
 
 function App() {
   return (
     <>
-      <h1 className="m-4 p-3 text-3xl font-bold text-main">
+      <h1 className="m-4 p-3 text-3xl font-bold text-secondary">
         Hello Mascotopia Frontend!
       </h1>
+      <Button />
     </>
   );
 }
---
+++ view/src/components/Button/Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <button className="rounded-lg bg-primary px-4 py-1 text-black hover:bg-main hover:text-primary">
+      Button
+    </button>
+  );
+};
+
+export default Button;
