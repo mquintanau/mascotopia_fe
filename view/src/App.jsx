07f8184e662df76c34c6e7a2efb60e003b4ff49e
Carlos Ramírez <cadavid4003@gmail.com>
2024-03-25 19:07:20 -0500

    Inicializa sistema de diseño con tailwind

--- view/src/App.jsx
+++ view/src/App.jsx
@@ -3,7 +3,9 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold">Hello Mascotopia Frontend!</h1>
+      <h1 className="text-3xl font-bold text-primary">
+        Hello Mascotopia Frontend!
+      </h1>
     </>
   );
 }
