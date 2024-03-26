bf60655083d5bb56d36cc6ed34b56cb4441d3570
Carlos Ramírez <cadavid4003@gmail.com>
2024-03-25 19:37:39 -0500

    Añade plugin prettier-plugin-tailwindcss

--- view/src/App.jsx
+++ view/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold text-primary">
+      <h1 className="m-4 p-3 text-3xl font-bold text-primary">
         Hello Mascotopia Frontend!
       </h1>
     </>
