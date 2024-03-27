21b5c2791dd261af3f45cd61f4c27a8ea889f699
Carlos Ramírez <cadavid4003@gmail.com>
2024-03-27 10:12:22 -0500

    Refactoriza component App

--- view/src/App.jsx
+++ view/src/App.jsx
@@ -4,17 +4,21 @@ import RectangularLogo from "./components/RectangularLogo/RectangularLogo";
 function App() {
   return (
     <div className="flex w-screen flex-col items-center justify-center bg-main">
-      <h1 className="m-0 p-3 text-3xl font-bold text-black">
-        Hello Mascotopia Frontend!
-      </h1>
-      <RectangularLogo className="h-[200px] w-[400px]" />
-      <Button
-        onClick={() => {
-          console.log("Click en botón!");
-        }}
-      >
-        Este es un boton nuevo
-      </Button>
+      <header></header>
+      <main>
+        <h1 className="m-0 p-3 text-3xl font-bold text-black">
+          Hello Mascotopia Frontend!
+        </h1>
+        <RectangularLogo className="h-[200px] w-[400px]" />
+        <Button
+          onClick={() => {
+            console.log("Click en botón!");
+          }}
+        >
+          Este es un boton nuevo
+        </Button>
+      </main>
+      <footer></footer>
     </div>
   );
 }
