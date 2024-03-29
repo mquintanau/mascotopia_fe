17a78da77c7fa8c7035a14fd3d2ef818d829bc99
Carlos Ramírez <cadavid4003@gmail.com>
2024-03-28 21:55:16 -0500

    Refactoriza el componente app, design system y el logo

--- view/src/App.jsx
+++ view/src/App.jsx
@@ -1,22 +1,27 @@
 import Button from "./components/Button/Button";
+import Navbar from "./components/Navbar/Navbar";
 import RectangularLogo from "./components/RectangularLogo/RectangularLogo";
 
 function App() {
   return (
-    <div className="flex w-screen flex-col items-center justify-center bg-main">
-      <header></header>
-      <main>
-        <h1 className="m-0 p-3 text-3xl font-bold text-black">
-          Hello Mascotopia Frontend!
-        </h1>
-        <RectangularLogo className="h-[200px] w-[400px]" />
-        <Button
-          onClick={() => {
-            console.log("Click en botón!");
-          }}
-        >
-          Este es un boton nuevo
-        </Button>
+    <div className="h-screen w-screen bg-main">
+      <header>
+        <Navbar />
+      </header>
+      <main className="h-[400px]">
+        <div className="flex h-full flex-col items-center justify-center">
+          <h1 className="m-0 p-3 text-3xl font-bold text-black">
+            Hello Mascotopia Frontend!
+          </h1>
+          <RectangularLogo className="h-[200px] w-[400px]" />
+          <Button
+            onClick={() => {
+              console.log("Click en botón!");
+            }}
+          >
+            Este es un boton nuevo
+          </Button>
+        </div>
       </main>
       <footer></footer>
     </div>
--- view/src/components/RectangularLogo/RectangularLogo.jsx
+++ view/src/components/RectangularLogo/RectangularLogo.jsx
@@ -3,7 +3,11 @@ import PropTypes from "prop-types";
 const RectangularLogo = ({ className, ...props }) => {
   return (
     <div className={`flex items-center justify-center ${className}`} {...props}>
-      <img src="https://i.ibb.co/R2H7816/image.png" alt="Logo" />
+      <img
+        src="https://i.ibb.co/R2H7816/image.png"
+        alt="Logo"
+        className="h-full w-full object-contain"
+      />
     </div>
   );
 };
