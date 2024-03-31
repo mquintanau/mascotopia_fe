0f983c29db1798238b608c0bba0209a57d45b77d
Carlos Ramírez <cadavid4003@gmail.com>
2024-03-31 18:48:47 -0500

    Mejora estilos boton y navbar

--- view/src/components/Button/Button.jsx
+++ view/src/components/Button/Button.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 const Button = ({ className = "", onClick, children, ...props }) => {
   return (
     <button
-      className={`rounded-lg bg-primary px-4 py-1 font-bold text-black hover:bg-black hover:text-primary ${className}`}
+      className={`rounded-lg bg-primary px-4 py-1 font-bold text-black hover:bg-black hover:text-primary focus:ring focus:ring-green2 ${className}`}
       onClick={onClick}
       {...props}
     >
