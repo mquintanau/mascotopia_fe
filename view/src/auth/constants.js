df6b21d06768410051a2b0950ca7263abb64b41a
Carlos Ramírez <cadavid4003@gmail.com>
2024-04-01 22:13:22 -0500

    Configura controlador para inicio de sesion básico

---
+++ view/src/auth/constants.js
@@ -0,0 +1,1 @@
+export const API_URL = 'http://localhost:4000/api'; //Se exporta la URL de la API para poder ser utilizada en otros archivos
--- view/src/pages/Login.jsx
+++ view/src/pages/Login.jsx
@@ -1,19 +1,79 @@
+import { useState } from "react";
+// Componentes propios de la marca
 import Button from "../components/Button/Button";
 import Input from "../components/Input/Input";
 import RectangularLogo from "../components/RectangularLogo/RectangularLogo";
+// Imágenes
 import LoginDog from "../assets/loginDog.png";
 import LoginLine from "../assets/loginLine.png";
+// Inicio de sesión
+import { API_URL } from "../auth/constants";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../auth/AuthProvider";
 
 function Login() {
+  // Variables de estado formulario
+  const [correo, setCorreo] = useState('');
+  const [contraseña, setContraseña] = useState('');
+  // Se inicializa el estado de la respuesta de error 
+  const [errorResponse, setErrorResponse] = useState(""); 
+  // Se inicializa la variable de navegación
+  const goTo = useNavigate();
+  // Se obtiene la función de autenticación
+  const auth = useAuth();
+
+  // Función que se ejecuta al enviar el formulario
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try{
+        const response = await fetch(`${API_URL}/login`, {//Se realiza una petición POST al servidor y se espera la respuesta
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ //Se envian los datos del formulario en formato JSON al servidor
+                correo,
+                contraseña
+            })
+        });
+        if(response.ok){
+          console.log('Login successful'); // TODO Reemplazar por sweetaXlert
+          setErrorResponse("");//Se limpia el estado de la respuesta de error
+          const json = await response.json();
+          if(json && json.body && json.body.user && json.body.accessToken && json.body.refreshToken){
+              auth.saveUser(json);
+              goTo("/perfil");
+          }
+        }
+        else{
+            console.log('Something went wrong');
+            const json = await response.json();
+            if (json && json.body && typeof json.body.error === 'string') {
+                setErrorResponse(json.body.error);
+                // json tiene la estructura de AuthResponseError
+            }
+        }
+    }catch(error){
+        console.log(error);
+    }
+  }
+
   return (
     <>
       <form
         action=""
+        onSubmit={handleSubmit}
         className="mx-auto mt-6 flex max-w-sm flex-col rounded-xl bg-navbar p-10"
       >
         <RectangularLogo className="m-auto mb-5 w-3/4 translate-x-3" />
-        <Input type="text" label="Email/Nombre de usuario" id="user" />
-        <Input type="password" label="Contraseña" id="pass" className="mt-5" />
+        {!!errorResponse && (
+            <div className="errorMessage bg-red-400 text-white p-2 rounded-md mb-4">
+              {errorResponse}
+            </div>
+        )}
+        <Input type="text" label="Email/Nombre de usuario" id="user" value={correo} onChange={(e) => setCorreo(e.target.value)} />
+        <Input type="password" label="Contraseña" id="pass" className="mt-5" value={contraseña} onChange={(e) => setContraseña(e.target.value)} />
 
         <a
           href=""
