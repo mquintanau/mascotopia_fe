92031e8bb8d01fc3e71b04d9b192e7c7f192182e
JuanSE <jumadridc@unal.edu.co>
2024-04-01 21:28:26 -0500

    Inicializa backend login

---
+++ backend/routes/login.js
@@ -0,0 +1,38 @@
+const { jsonResponse } = require("../lib/jsonResponse");
+const User = require("../schema/user")//importamos el modelo de usuario
+const router = require("express").Router()//importamos el router de express
+const getUserInfo = require("../lib/getUserInfo");//importamos la funcion para obtener la informacion del usuario
+
+router.post('/', async (req, res) => {
+    const {correo, contraseña} = req.body;//obtenemos los datos del body
+    if(!!!correo ||  !!!contraseña) {//verificamos si los campos estan vacios
+        return res.status(400).json(jsonResponse(400,{//retornamos un json con el mensaje de error
+            error: "Los campos son requeridos"
+        })
+        );
+    }
+
+    //Autenticar usuario en la base de datos
+    const user= await User.findOne({correo});//buscamos un usuario con el correo proporcionado
+    if (user){//si el usuario existe
+        const correctPassword = await user.comparePassword(contraseña, user.contraseña);//comparamos la contraseña en texto plano con la contraseña encriptada
+        if (correctPassword){//si la contraseña es correcta
+            const accessToken = user.createAccessToken();//creamos el token de acceso
+            const refreshToken = await user.createRefreshToken();//creamos el token de refresco
+            return res.status(200).json(jsonResponse(200,{//retornamos un json con el mensaje de exito
+                user: getUserInfo(user),accessToken,refreshToken
+            }));
+        }else{//si la contraseña es incorrecta
+            return res.status(400).json(jsonResponse(400,{//retornamos un json con el mensaje de error
+                error: "El correo o la contraseña es incorrecto"
+            }));
+        }
+    }else{//si el usuario no existe
+        return res.status(404).json(jsonResponse(404,{//retornamos un json con el mensaje de error
+            error: "Correo o contraseña  no encontrados"
+        }));
+    }
+});
+
+
+module.exports = router; //exportamos el router para que pueda ser utilizado en otros archivos
---
+++ backend/schema/user.js
@@ -0,0 +1,58 @@
+const mongoose= require('mongoose');//importamos la libreria mongoose para manejar los esquemas
+const bcrypt = require('bcrypt');//importamos la libreria bcrypt para encriptar la contraseña
+const { generateAccessToken,generateRefreshToken } = require('../auth/generateTokens'); //importamos la funcion para generar el token de acceso
+const Token = require('../schema/token');//importamos el modelo de token
+const getUserInfo = require('../lib/getUserInfo');//importamos la funcion para obtener la informacion del usuario
+
+const UserSchema = new mongoose.Schema({ //creamos el esquema de usuario
+    id: { type: Object},
+    correo: { type: String, required: true, unique:true},
+    contraseña: { type: String, required: true},
+    nombre: { type: String, required: true},
+});
+
+//RECORDAR QUE LOS DOCUMENTOS SON LAS TABLAS EN MONGODB
+
+UserSchema.pre('save', function(next){//funcion que se ejecuta antes de guardar el documento
+    if(this.isNew || this.isModified('contraseña')){//si el documento es nuevo o la contraseña ha sido modificada
+        const document = this;//guardamos el documento
+        bcrypt.hash(document.contraseña, 10, (err, hash) => {//encriptamos la contraseña
+            if(err){
+                next(err);//si hay un error, lo pasamos al siguiente middleware
+            }else{
+                document.contraseña = hash;//si no hay error, guardamos la contraseña encriptada
+                next();//pasamos al siguiente middleware
+            }
+        });
+    }else{
+        next();//si no es nuevo ni se ha modificado la contraseña, pasamos al siguiente middleware
+    }
+});
+//Se utiliza next ya que los middleware son funciones que se ejecutan en pila, es decir, una tras otra, y next es la función que se utiliza para pasar al siguiente middleware
+
+UserSchema.methods.correoExist = async function(correo){ //funcion para verificar si el correo ya existe
+    const result = await mongoose.model('User').findOne({correo}); //buscamos un usuario con el correo proporcionado
+    return !!result; //retornamos si el usuario existe o no
+}
+
+UserSchema.methods.comparePassword = async function(contraseña, hash){//funcion para comparar la contraseña encriptada con la contraseña en texto plano
+    const same = await bcrypt.compare(contraseña, hash);//comparamos la contraseña en texto plano con la contraseña encriptada
+    return same;//retornamos si son iguales o no
+}
+
+
+UserSchema.methods.createAccessToken = function(){
+    return generateAccessToken(getUserInfo(this));
+}
+
+UserSchema.methods.createRefreshToken = async function(){
+    const refreshToken = generateRefreshToken(getUserInfo(this));//creamos el token de refresco
+    try{
+        await new Token({token:refreshToken}).save();
+        return refreshToken;
+    }catch(error){
+        console.log(error);
+    }
+}
+
+module.exports = mongoose.model('User', UserSchema);//exportamos el modelo de usuario
