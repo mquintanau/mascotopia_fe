09aa3eccbb8883d235fa52b79c11760be51d5890
Carlos Ramírez <cadavid4003@gmail.com>
2024-04-01 19:57:03 -0500

    Elimina overflow horizontal

--- view/src/pages/Login.jsx
+++ view/src/pages/Login.jsx
@@ -6,33 +6,35 @@ import LoginLine from "../assets/loginLine.png";
 
 function Login() {
   return (
-    <form
-      action=""
-      className="mx-auto mt-6 flex max-w-sm flex-col rounded-xl bg-navbar p-10"
-    >
-      <RectangularLogo className="m-auto mb-5 w-3/4 translate-x-3" />
-      <Input type="text" label="Email/Nombre de usuario" id="user" />
-      <Input type="password" label="Contraseña" id="pass" className="mt-5" />
-
-      <a
-        href=""
-        className="text-center font-light text-black hover:text-greenLogo active:font-normal"
+    <>
+      <form
+        action=""
+        className="mx-auto mt-6 flex max-w-sm flex-col rounded-xl bg-navbar p-10"
       >
-        ¿Olvidaste tu contraseña?
-      </a>
+        <RectangularLogo className="m-auto mb-5 w-3/4 translate-x-3" />
+        <Input type="text" label="Email/Nombre de usuario" id="user" />
+        <Input type="password" label="Contraseña" id="pass" className="mt-5" />
 
-      <Button type="submit" className="mx-auto my-5 whitespace-nowrap">
-        Iniciar Sesión
-      </Button>
-
-      <hr className="my-5 border-black" />
-      <p className="text-center font-light">
-        ¿No tienes una cuenta?{" "}
-        <a href="" className="font-semibold text-black hover:text-greenLogo">
-          Regístrate
+        <a
+          href=""
+          className="text-center font-light text-black hover:text-greenLogo active:font-normal"
+        >
+          ¿Olvidaste tu contraseña?
         </a>
-      </p>
-    </form>
+
+        <Button type="submit" className="mx-auto my-5 whitespace-nowrap">
+          Iniciar Sesión
+        </Button>
+
+        <hr className="my-5 border-black" />
+        <p className="text-center font-light">
+          ¿No tienes una cuenta?{" "}
+          <a href="" className="font-semibold text-black hover:text-greenLogo">
+            Regístrate
+          </a>
+        </p>
+      </form>
+    </>
   );
 }
 
