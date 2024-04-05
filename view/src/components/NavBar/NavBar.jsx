930c774a17f09b797bf2cea8dacc97596d643f99
laurad08412 <lauraagude084@gmail.com>
2024-04-05 15:42:50 -0500

    Hacer la barra NavBar responsive

--- view/src/components/NavBar/NavBar.jsx
+++ view/src/components/NavBar/NavBar.jsx
@@ -1,12 +1,19 @@
+import { useState } from 'react';
 import UserNavbarImage from "../User/UserNavbarImage/UserNavbarImage";
 
 const NavBar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <div className="flex items-center justify-between">
       <div className="fixed left-0">
         <UserNavbarImage className="ml-[22px]" />
       </div>
-      <div className="fixed right-0">
+      <div className="fixed right-0 lg:flex hidden">
         <a
           href="#"
           className="mr-[25px]"
@@ -34,6 +41,48 @@ const NavBar = () => {
         >
           Calendar
         </a>
+      </div>
+      <div className="absolute right-0 px-2 lg:hidden">
+        <button onClick={toggleMenu}>
+          
+            {showMenu ? (
+              <img src="/shared/Arrow.svg" className="w-[17px] animate-fadeIn" alt="Logo"/>
+            ) : (
+              <img src="/shared/Hamburger icon.svg" className="w-[20px] animate-bounce" alt="Logo"/>
+            )}
+          
+        </button>
+        {showMenu && (
+          <div className="absolute top-9 right-0 bg-white w-screen border rounded shadow-lg">
+            <a
+              href="#"
+              className="block px-4 py-2 text-black  hover:bg-gray-100"
+              onClick={() => {
+                console.log("Click en Noticias");
+              }}
+            >
+              News
+            </a>
+            <a
+              href="#"
+              className="block px-4 py-2 text-gray-800 hover:bg-gray-100"
+              onClick={() => {
+                console.log("Click en Foros");
+              }}
+            >
+              Forums
+            </a>
+            <a
+              href="#"
+              className="block px-4 py-2 text-gray-800 hover:bg-gray-100"
+              onClick={() => {
+                console.log("Click en Calendario");
+              }}
+            >
+              Calendar
+            </a>
+          </div>
+        )}
       </div>
     </div>
   );
