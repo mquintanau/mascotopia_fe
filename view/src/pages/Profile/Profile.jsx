e06ffa2952cbf100d6957b2afc8e8d1d25c1c7aa
laurad08412 <lauraagude084@gmail.com>
2024-04-10 00:50:43 -0500

    Crear funcion para poner vistas de mascotas dependiendo de los datos del usuario

--- view/src/pages/Profile/Profile.jsx
+++ view/src/pages/Profile/Profile.jsx
@@ -1,10 +1,47 @@
 import FooterRectangle from "../../components/FooterRectangle/FooterRectangle";
 import NavbarInternal from "../../components/NavbarInternal/NavbarInternal";
 import PetView from "../../components/Pet/PetView/PetView";
+import PetList from "../../components/PetList/Petlist";
 import UserView from "../../components/User/UserView/UserView";
 
 //Ejemplos para la pagina mientras union
 //Revisar como unir pet y user
+//Ejemplos para la pagina mientras union
+const petsData = [
+  {
+    imageURL: "./shared/EjemploPet.jpg",
+    petLocation: "Bogotá",
+    name: "Fluffy",
+    age: 3,
+    kind: "Dog",
+    description: "A fluffy and friendly dog.",
+  },
+  {
+    imageURL: "https://via.placeholder.com/150",
+    petLocation: "Los Angeles",
+    name: "Whiskers",
+    age: 2,
+    kind: "Cat",
+    description: "Independent and curious",
+  },
+  {
+    imageURL: "https://via.placeholder.com/150",
+    petLocation: "Chicago",
+    name: "Buddy",
+    age: 5,
+    kind: "Dog",
+    description: "Loyal and energetic",
+  },
+  {
+    imageURL: "https://via.placeholder.com/150",
+    petLocation: "Houston",
+    name: "Fluffy",
+    age: 1,
+    kind: "Rabbit",
+    description: "Adorable and cuddly",
+  },
+];
+
 const user = {
   name: "AAAaaaA css",
   email: "aaa@gmail.com",
@@ -15,19 +52,9 @@ const user = {
   birthday: "1990-06-15",
   contact_number: 1111111111,
   description: ":3",
-  number_pets: 2
+  number_pets: 2,
 };
 
-//Ejemplos para la pagina mientras union
-const pet = {
-  owner_id: 20,
-  imageURL: "./shared/EjemploPet.jpg",
-  petLocation: "Bogotá",
-  name: "Fluffy",
-  age: 3,
-  kind: "Dog",
-  description: "A fluffy and friendly dog.",
-};
 
 const Profile = () => {
   return (
@@ -52,22 +79,7 @@ const Profile = () => {
           </div>
           <div className="m-6">
             <div className="flex flex-col">
-              <PetView
-                imageURL={pet.imageURL}
-                petLocation={pet.petLocation}
-                petName={pet.name}
-                petAge={pet.age}
-                animalPet={pet.kind}
-                petDescription={pet.description}
-              />
-              <PetView
-                imageURL={pet.imageURL}
-                petLocation={pet.petLocation}
-                petName={pet.name}
-                petAge={pet.age}
-                animalPet={pet.kind}
-                petDescription={pet.description}
-              />
+              <PetList pets={petsData} />
               {/* Se ponen mas mascotas dependiendo de la cantidad de mascotas del usuario */}
             </div>
           </div>
