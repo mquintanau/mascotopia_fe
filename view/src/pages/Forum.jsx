eff0190e52ef98caf60fa1afa3420ce114c77253
JuanSE <jumadridc@unal.edu.co>
2024-04-17 12:34:29 -0500

    Arregla el ancho de los contenedores

--- view/src/pages/Forum.jsx
+++ view/src/pages/Forum.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 function Forum() {
     return (
         <div className="h-screen flex flex-row items-start p-5" >
-            <div className="bg-secondary p-6 rounded-xl w-80 h-[35rem] ml-28">
+            <div className="bg-secondary p-6 rounded-xl h-[35rem] ml-28 w-96">
 
             </div>
 
-            <div className="bg-green3 p-6 rounded-xl flex-grow w-80 h-[35rem] mr-28">
+            <div className="bg-green3 p-6 rounded-xl flex-grow h-[35rem] mr-28">
                 HOLA CARLOS
             </div>
         </div>
