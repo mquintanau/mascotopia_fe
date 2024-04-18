a74ca1639209ca5c0837e0f58bb62e469b1260c2
JuanSE <jumadridc@unal.edu.co>
2024-04-17 19:23:20 -0500

    Mejora buscador visualmente

--- view/src/components/Input/Input.jsx
+++ view/src/components/Input/Input.jsx
@@ -1,17 +1,17 @@
 import PropTypes from "prop-types";
 
-const Input = ({ id, label, className, ...props }) => {
+const Input = ({ id, label, className,inputClassName,labelClassName,  ...props }) => {
   return (
     <div className={`relative my-3 w-full ${className}`}>
       <input
         id={id}
-        className={`peer w-full rounded-2xl bg-main px-3 py-2.5 placeholder-transparent`}
+        className={`peer w-full rounded-2xl bg-main px-3 py-2.5 placeholder-transparent ${inputClassName}`}
         placeholder={label}
         {...props}
       />
       <label
         htmlFor={id}
-        className="absolute -top-6 left-0 text-sm text-gray-600 transition-all peer-placeholder-shown:left-3 peer-placeholder-shown:top-2.5 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-focus:-top-6 peer-focus:left-0 peer-focus:text-sm peer-focus:text-gray-600"
+        className={`absolute -top-6 left-0 text-sm text-gray-600 transition-all peer-placeholder-shown:left-3 peer-placeholder-shown:top-2.5 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-focus:-top-6 peer-focus:left-0 peer-focus:text-sm peer-focus:text-gray-600 ${labelClassName}`}
       >
         {label}
       </label>
@@ -23,6 +23,8 @@ Input.propTypes = {
   id: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
   className: PropTypes.string,
+  inputClassName: PropTypes.string,
+  labelClassName: PropTypes.string,
 };
 
 export default Input;
--- view/src/pages/Forum.jsx
+++ view/src/pages/Forum.jsx
@@ -2,6 +2,8 @@ import  Input  from '../components/Input/Input';
 import React, { useState } from 'react';
 import Topic from '../components/Topic/Topic';
 import QuestionList from '../components/QuestionList/QuestionList';
+import Search from "../assets/Search.png";
+
 
 const questionTest = [
     {
@@ -37,13 +39,20 @@ function Forum() {
             <div className="bg-secondary p-6 rounded-xl h-[35rem] ml-28 w-96 overflow-auto">
                 <h1 className="text-4xl mt-2 mb-3">Last Topics {'>'} </h1>
                 <hr className="border-black mr-[-1.5rem]"></hr>
-                <Input
-                    id="search"
-                    label="Search Forum"
-                    className="rounded-sm"
-                />
+                <div className='flex flex-row items-start w-full mt-3'>
+                    <button className="w-5 h-5 mx-2 translate-y-3.5">
+                        <img src={Search}/>
+                    </button>
+                    <Input
+                        type="text"
+                        id="search"
+                        label= "Search Forum"
+                        inputClassName= "py-0 rounded-md"
+                        labelClassName= "translate-y-[-35%]"
+                    />
+                </div>
 
-                <ol className="mt-2">
+                <ol className="mt-0">
                     <li>
                         <Topic
                             name="Topic 1 >"
