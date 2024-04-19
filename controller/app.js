3d53af64f42aa94b79cebe8363a6e0c9ae96457d
Carlos Ramirez <cramirezmun@unal.edu.co>
2024-04-19 12:18:57 -0500

    Refactoriza app.js en el controlador

--- controller/app.js
+++ controller/app.js
@@ -1,35 +1,37 @@
-const express = require('express'); 
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
+require("dotenv").config();
 
-require('dotenv').config();
-
-const port = process.env.PORT || 4000;//puerto del servidor
-app.use(cors());//permite que el servidor acepte peticiones de cualquier origen
-app.use(express.json());//permite que el servidor pueda recibir y enviar datos en formato JSON
-
+const port = process.env.PORT || 4000; //puerto del servidor
+app.use(cors()); //permite que el servidor acepte peticiones de cualquier origen
+app.use(express.json()); //permite que el servidor pueda recibir y enviar datos en formato JSON
 
 //CONEXIÓN CON LA BASE DE DATOS
-async function connectDB(){
-    await mongoose.connect(process.env.DB_CONNECTION_STRING)
-    console.log('DB connected')
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.DB_CONNECTION_STRING);
+    console.log("DB connected");
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-connectDB().catch(console.eror);
+connectDB();
 
 //RUTAS
-app.use('/api/login', require('./routes/login'));
-app.use('/api/userProfile', require('./routes/userProfile'));
-app.use('/api/signup', require('./routes/signup')); 
-app.use('/api/forgotPassword', require('./routes/forgotPassword')); 
-app.use('/api/resetPassword', require('./routes/resetPassword'));
+app.use("/api/login", require("./routes/login"));
+app.use("/api/userProfile", require("./routes/userProfile"));
+app.use("/api/signup", require("./routes/signup"));
+app.use("/api/forgotPassword", require("./routes/forgotPassword"));
+app.use("/api/resetPassword", require("./routes/resetPassword"));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
