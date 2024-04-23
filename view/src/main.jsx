439f842abd1f2fbc91bbd4621ad23a19eb3cfcd3
Carlos Ramirez <cramirezmun@unal.edu.co>
2024-04-23 02:50:09 +0000

    Revierte "calendar"
    Revierte el commit e500cc55e69dcceb4d5a2a8c8f32bb5dc26f7dd5.

--- view/src/main.jsx
+++ view/src/main.jsx
@@ -4,7 +4,6 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./auth/AuthProvider";
-import Model from 'react-model';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
