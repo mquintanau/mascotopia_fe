4825482e72952113b4ee3e90412c71c077361480
ValenChi <dchicuasuque@unal.edu.co>
2024-04-24 13:25:41 -0500

    calendar6
    calendario emergente listo

--- view/src/pages/AddEventModal.jsx
+++ view/src/pages/AddEventModal.jsx
@@ -1,44 +1,66 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import Modal from "react-modal";
-import Datetime from 'react-datetime'
-export default function ({isOpen, onClose,onEventAdded}){
-    const [title,setTitle] = useState("");
-    const [start,setStart] = useState(new Date());
-    const [end,setEnd] = useState(new Date());
-    const [description,setDescription] = useState("");
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+export default function AddEventModal({ isOpen, onClose, onEventAdded }) {
+    const [title, setTitle] = useState("");
+    const [start, setStart] = useState(new Date());
+    const [end, setEnd] = useState(new Date());
+    const [description, setDescription] = useState("");
 
     const onSubmit = (event) => {
         event.preventDefault();
 
         onEventAdded({
-
             start,
             end,
             title,
             description
-        })
-        onClose();
-    }
+        });
 
-    return(
+        onClose();
+    };
+
+    return (
         <Modal isOpen={isOpen} onRequestClose={onClose}>
             <form onSubmit={onSubmit}>
-                <input placeholder="Ingrese el titulo del evento" value={title} onChange={e => setTitle(e.target.value)}/>
+                <input
+                    placeholder="Ingrese el titulo del evento"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
                 <div>
-                    <input placeholder="Ingrese la descripcion del evento" value={description} onChange={e => setDescription(e.target.value)}/>
+                    <input
+                        placeholder="Ingrese la descripcion del evento"
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                    />
                 </div>
                 <div>
                     <label>Start Date</label>
-                    <Datetime value={start} onChange={date=> setStart(date)} />
+                    <DatePicker
+                        selected={start}
+                        onChange={(date) => setStart(date)}
+                        showTimeSelect
+                        timeFormat="HH:mm"
+                        timeIntervals={15}
+                        dateFormat="MMMM d, yyyy h:mm aa"
+                    />
                 </div>
                 <div>
                     <label>End Date</label>
-                    <Datetime value={end} onChange={date=> setEnd(date)} />
+                    <DatePicker
+                        selected={end}
+                        onChange={(date) => setEnd(date)}
+                        showTimeSelect
+                        timeFormat="HH:mm"
+                        timeIntervals={15}
+                        dateFormat="MMMM d, yyyy h:mm aa"
+                    />
                 </div>
                 <button>Add event</button>
-                
             </form>
-
         </Modal>
-    )
+    );
 }
