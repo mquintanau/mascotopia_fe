59332e26369480bb2a02b286a5abb0fdbf528bc3
JuanSE <jumadridc@unal.edu.co>
2024-04-25 12:06:03 -0500

    Agrega funcionalidad responsive a la segunda vista del foro

--- view/src/pages/QuestionView.jsx
+++ view/src/pages/QuestionView.jsx
@@ -1,81 +1,92 @@
-import { Link } from 'react-router-dom';
-import QuestionList from '../components/QuestionList/QuestionList';
-import { useState } from 'react';
-import Button from '../components/Button/Button'
+import { Link } from "react-router-dom";
+import QuestionList from "../components/QuestionList/QuestionList";
+import { useState } from "react";
+import Button from "../components/Button/Button";
 import Blob from "../assets/Blob.png";
-
 
 const QuestionView = () => {
   // Se almacena la pregunta seleccionada en la constante selectedQuestion
-  const [selectedQuestion , setSelectedQuestion] = useState(null);
+  const [selectedQuestion, setSelectedQuestion] = useState(null);
 
   // Preguntas de prueba
   const questionTest = [
     {
-    name : "Pablo",
-    description : "Hola, como estas?"
+      name: "Pablo",
+      description: "Hola, como estas?",
     },
     {
-    name : "Juan",
-    description : "Bien, gracias y tu?"
+      name: "Juan",
+      description: "Bien, gracias y tu?",
     },
     {
-    name : "Pablo",
-    description : "Bien, gracias"    
+      name: "Pablo",
+      description: "Bien, gracias",
     },
     {
-        name : "Pablo",
-        description : "Bien, gracias"    
+      name: "Pablo",
+      description: "Bien, gracias",
     },
-];
+  ];
 
-// Funcion que establece la pregunta seleccionada con sus atributos de titulo y descripcion
-const handleQuestionSelect = (question) => {
+  // Funcion que establece la pregunta seleccionada con sus atributos de titulo y descripcion
+  const handleQuestionSelect = (question) => {
     setSelectedQuestion(question);
   };
 
   // Se retorna un div con la lista de preguntas y la pregunta seleccionada
   return (
-    <div className=" flex flex-row items-start h-screen p-5"
-    style={{
-      backgroundImage: `url(${Blob})`,
-      backgroundPosition: '50%', 
-      backgroundRepeat: 'no-repeat', 
-    }}>
-      
+    <div
+      className=" flex min-h-screen flex-col items-start p-5 lg:flex-row"
+      style={{
+        backgroundImage: `url(${Blob})`,
+        backgroundPosition: "50%",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
       {/* Se muestra un div con la lista de preguntas y establece que pasa cuando se da click en Questions */}
-      <div className="bg-green3 p-6 rounded-xl h-[35rem] ml-28 w-[40rem] overflow-auto">
-      <Link to="/forum">
-        <h1 className="text-4xl mt-2 mb-3 hover:text-teal">{'<'} Questions</h1>
-      </Link>
-      <hr className="border-black mb-5"></hr>
-      <QuestionList questions={questionTest} onQuestionSelect={handleQuestionSelect} />
+      <div className="mb-7 h-screen max-h-[500px] w-full overflow-auto rounded-xl bg-green3 p-6 lg:ml-28 lg:max-h-full lg:w-1/2">
+        <Link to="/forum">
+          <h1 className="mb-3 mt-2 text-2xl hover:text-teal lg:text-4xl">
+            {"<"} Questions
+          </h1>
+        </Link>
+        <hr className="mb-5 border-black"></hr>
+        <QuestionList
+          questions={questionTest}
+          onQuestionSelect={handleQuestionSelect}
+        />
       </div>
 
       {/* Se muestra un div con la pregunta seleccionada */}
-      <div  className="bg-gray1 p-6 rounded-xl flex-grow h-[35rem] mr-28 overflow-auto"  >
+      <div className="h-screen max-h-[500px] w-full overflow-auto rounded-xl bg-gray1 p-6 lg:mr-28 lg:max-h-full lg:w-1/2">
         {/*Si existe selectedQuestion muestra un div con el formato de la pregunta al lado derecho de la lista de preguntas*/}
         {selectedQuestion && (
-          <div className='flex flex-col h-full '> 
-            <div className="bg-white mb-4 rounded-xl px-5 pb-6 pt-2 basis-3/5">
+          <div className="flex h-full flex-col ">
+            <div className="mb-4 basis-3/5 rounded-xl bg-white px-5 pb-6 pt-2">
               <h3>{selectedQuestion.name}</h3>
-              <h3>Question:   ...</h3>
+              <h3 className="text-sm lg:text-xl">Question: ...</h3>
               <p className="font-light">{selectedQuestion.description}</p>
             </div>
-            <div className="bg-green5 rounded-xl basis-2/5 px-5">
-            <form>
-              <div>
-                <p className='mt-5'>Add a Comment: </p>
-                <textarea type="text" className='mt-3  h-32 rounded-xl w-full py-2 px-4 bg-main' placeholder='Write Here' />
-              </div>
-              <Button type="submit" className="mx-auto mb-5 whitespace-nowrap  px-6 py-2 bg-secondary rounded-3xl font-normal">
-                Send
-              </Button>
+            <div className="basis-2/5 rounded-xl bg-green5 px-5">
+              <form>
+                <div>
+                  <p className="mt-5">Add a Comment: </p>
+                  <textarea
+                    type="text"
+                    className="mt-3  h-32 w-full rounded-xl bg-main px-4 py-2"
+                    placeholder="Write Here"
+                  />
+                </div>
+                <Button
+                  type="submit"
+                  className="mx-auto mb-5 whitespace-nowrap  rounded-3xl bg-secondary px-6 py-2 font-normal"
+                >
+                  Send
+                </Button>
               </form>
             </div>
           </div>
         )}
-
       </div>
     </div>
   );
