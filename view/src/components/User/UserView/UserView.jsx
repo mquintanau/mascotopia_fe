163d1f6920805670e58649f5583483f2b78e4eb5
Carlos Ramirez <cramirezmun@unal.edu.co>
2024-05-07 23:36:10 -0500

    Mejora estilos seccion sobre ti

--- view/src/components/User/UserView/UserView.jsx
+++ view/src/components/User/UserView/UserView.jsx
@@ -1,9 +1,11 @@
 import PropTypes from "prop-types";
 const UserView = (props) => {
   const { name, email, username, role, number_pets, imageURL } = props;
+  const roleFullName = role === "petOwner" ? "Pet Owner" : "Volunteer";
+
   return (
     <div className="items-start font-semibold text-black">
-      <p className="flex justify-start px-4 py-1">User</p>
+      <p className="flex justify-start px-4 py-1">About You:</p>
       <div className="flex min-h-[450px] min-w-[300px] justify-center rounded-[13px] bg-[#A4F3B3]">
         <div className="my-2 flex flex-col items-center justify-center">
           <div className="relative mb-4 h-[175px] w-[175px] justify-center overflow-hidden rounded-full">
@@ -14,11 +16,22 @@ const UserView = (props) => {
           </div>
           <div className="flex-col justify-start border-t-4 border-primary px-4 text-[15px] font-light text-black">
             <div className="py-2 text-left leading-8">
-              <p>Name: {name}</p>
-              <p>Email: {email}</p>
-              <p>Username: {username}</p>
-              <p>Pets: {number_pets}</p>
-              <p> &gt; {role} </p>
+              <p>
+                <span className="font-bold">Name:</span> {name}
+              </p>
+              <p>
+                <span className="font-bold">Email:</span> {email}
+              </p>
+              <p>
+                <span className="font-bold">Username:</span> {username}
+              </p>
+              <p>
+                <span className="font-bold">Pets:</span> {number_pets}
+              </p>
+              <p>
+                {" "}
+                <span className="font-bold">Role &gt;</span> {roleFullName}{" "}
+              </p>
             </div>
           </div>
         </div>
