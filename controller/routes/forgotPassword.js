e47f1bc18ab989e4d10e5dfe9c0f771c8e37586c
Juan Madrid <jumadridc@unal.edu.co>
2024-05-11 15:42:47 -0500

    Agrega trazabilidad a Forgot Password y corrige envio de correo

--- controller/routes/forgotPassword.js
+++ controller/routes/forgotPassword.js
@@ -4,6 +4,7 @@ const router = require("express").Router(); //importamos el router de express
 const getUserInfo = require("../lib/getUserInfo"); //importamos la funcion para obtener la informacion del usuario
 const jwt = require("jsonwebtoken"); //importamos la libreria de jwt
 const nodemailer = require("nodemailer");
+const ActivityLog = require("../schema/ActivityLog"); //importamos el modelo de log de actividades
 require("dotenv").config();
 
 //verificamos si los campos estan vacios
@@ -54,12 +55,12 @@ router.post("/", async (req, res) => {
   });
 
   var mailOptions = {
-    from: "mascotopiapp@gmail.com", //correo de donde se envia
+    from: correo, //correo a donde se envia
     to: "juandamadrid7@gmail.com", //correo a donde se envia
     subject: "Reset Your Password", //asunto del correo
     text: `http://localhost:5173/resetPassword/${user._id}/${token}`, //Texto del correo
   };
-  transporter.sendMail(mailOptions, function (error, info) {
+  transporter.sendMail(mailOptions,async function (error, info) {
     if (error) {
       return res.status(500).json(
         jsonResponse(500, {
@@ -67,6 +68,16 @@ router.post("/", async (req, res) => {
         })
       );
     } else {
+      const newActivity = new ActivityLog({
+        //creamos un nuevo registro en el log de actividades
+        idUsuario: user._id,
+        nombre: user.nombre,
+        accion: "Forgot password",
+        fecha: new Date(),
+      });
+      
+      await newActivity.save(); //guardamos el registro en la base de datos
+
       return res.send({ Status: "Correo enviado" });
     }
   });
