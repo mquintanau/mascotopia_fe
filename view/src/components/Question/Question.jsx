f17b84399b42afc5408724d7246b33c64d76b9be
Juan Madrid <jumadridc@unal.edu.co>
2024-05-13 18:35:04 -0500

    Agrega botón delete y quita problemas de propagación

--- view/src/components/Question/Question.jsx
+++ view/src/components/Question/Question.jsx
@@ -1,32 +1,40 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import Button from "../Button/Button";
 
 // Componente que muestra una pregunta
 const Question = (props) => {
   // Se almacenan las props dadas a la pregunta en las siguientes constantes
   const { titulo, descripcion, onSelect, id, autor, idTopic, correo } = props;
-
+  const navigate = useNavigate();
   // Se retorna un link que redirige a la vista de la pregunta con el id de la pregunta
   return (
-    <Link to={`/questionView/${id}/${idTopic}`}>
-      <div
-        className="mb-4 rounded-xl bg-white px-5 pb-6 pt-2 text-sm hover:cursor-pointer hover:bg-gray-200 lg:text-xl"
-        // Se llama a la funcion onSelect con el titulo y descripcion de la pregunta
-        onClick={() => onSelect({ titulo, descripcion, autor })}
-      >
-        {/* Se muestra el titulo, descripcion y autor de la pregunta */}
-        <h3 className="capitalize">{titulo}</h3>
-        <h3>Question: {autor}</h3>
-        <p className="font-light">{descripcion}</p>
-        {correo === "admin@gmail.com" && (
-          <div className="mt-2 w-full">
-            <button className="admin-button bg-red-200">
-              Botón de administrador
-            </button>
-          </div>
-        )}
-      </div>
-    </Link>
+    <div
+      className="mb-4 rounded-xl bg-white px-5 pb-6 pt-2 text-sm hover:cursor-pointer hover:bg-gray-200 lg:text-xl"
+      onClick={() => {
+        onSelect({ titulo, descripcion, autor });
+        navigate(`/questionView/${id}/${idTopic}`);
+      }}
+    >
+      {/* Se muestra el titulo, descripcion y autor de la pregunta */}
+      <h3 className="capitalize">{titulo}</h3>
+      <h3>Question: {autor}</h3>
+      <p className="font-light">{descripcion}</p>
+      {correo === "admin@gmail.com" && (
+        <div className="mt-2 flex w-full items-end justify-end">
+          <Button
+            type="submit"
+            className="bg-red-300"
+            onClick={(e) => {
+              e.stopPropagation();
+              console.log("Delete");
+            }}
+          >
+            Delete
+          </Button>
+        </div>
+      )}
+    </div>
   );
 };
 
