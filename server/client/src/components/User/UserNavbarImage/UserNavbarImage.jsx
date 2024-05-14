e95c7a5423c964419c4e4b48be13ea31eae692dc
mquintanau <mquintanau@unal.edu.co>
2024-05-14 14:57:27 -0500

    Cambio de url para despliegue

--- server/client/src/components/User/UserNavbarImage/UserNavbarImage.jsx
+++ server/client/src/components/User/UserNavbarImage/UserNavbarImage.jsx
@@ -15,7 +15,7 @@ const UserNavbarImage = ({ className, ...props }) => {
     >
       {data && data.imageURL ? (
         <img
-          src={"http://localhost:4000" + data.imageURL}
+          src={"https://mascotopia-fe.onrender.com" + data.imageURL}
           className="h-10 w-10 rounded-full"
           alt="Avatar del usuario"
         />
