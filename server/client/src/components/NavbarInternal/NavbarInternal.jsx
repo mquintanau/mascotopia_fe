9ba08d2a73eb376b4f5ae8dd63ca0513947b449f
Juan Madrid <jumadridc@unal.edu.co>
2024-05-14 15:38:56 -0500

    Arregla ruta de calndario en responsive

--- server/client/src/components/NavbarInternal/NavbarInternal.jsx
+++ server/client/src/components/NavbarInternal/NavbarInternal.jsx
@@ -91,7 +91,7 @@ const NavbarInternal = () => {
               Forums
             </Link>
             <a
-              href="#"
+              href="/calendar"
               className="block px-4 py-2 text-gray-800 hover:bg-gray-100"
               onClick={() => console.log("Click en Calendario")}
             >
