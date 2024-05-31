53eb4dde7ccfaff674a700f543bf930dcf847e3d
Carlos Ramirez <cramirezmun@unal.edu.co>
2024-05-31 10:33:54 -0500

    Establece interfaz basica para el feed

--- server/client/src/pages/PostContainer.jsx
+++ server/client/src/pages/PostContainer.jsx
@@ -3,7 +3,7 @@ import PostItem from "./PostItem";
 const PostContainer = ({ posts }) => {
   console.log(posts.map((post) => post.id));
   return (
-    <div className="mx-auto max-w-[600px] bg-secondary px-10">
+    <div className="mx-auto my-6 max-w-[600px] rounded-lg bg-secondary p-10">
       Featured &amp; Local News &gt;
       {posts.map((post) => (
         <PostItem value={post} key={post.id} />
--- server/client/src/pages/PostItem.jsx
+++ server/client/src/pages/PostItem.jsx
@@ -1,10 +1,22 @@
+import { Heart } from "iconoir-react";
+
 const PostItem = ({ value }) => {
   const { title, description, numLikes } = value;
+  const imageUrl = value.imageUrl || "https://via.placeholder.com/150";
+
   return (
-    <div>
-      <h2>{title}</h2>
-      <p>{description}</p>
-      <p>Likes: {numLikes}</p>
+    <div className="my-7 flex flex-row flex-nowrap items-center justify-center overflow-hidden rounded-lg bg-white font-normal">
+      <div className="flex w-2/5 items-center justify-center">
+        <img src={imageUrl} alt={title} className="py-6" />
+      </div>
+      <div className="w-3/5 text-base">
+        <h2 className="font-bold">{title}</h2>
+        <p className="mt-1">{description}</p>
+        <p className="mt-1 flex flex-row">
+          <Heart />
+          <span className="ml-2">Likes: {numLikes}</span>
+        </p>
+      </div>
     </div>
   );
 };
