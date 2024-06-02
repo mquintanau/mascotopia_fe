4d3fdac2ce9106c768aa3ac37b4f59b0a5e83a2c
Juan Madrid <jumadridc@unal.edu.co>
2024-06-02 12:41:16 -0500

    Agrega carga de posts y configura la base de datos para aceptar la imagen del usuario que publica

--- server/client/src/components/Post/PostContainer.jsx
+++ server/client/src/components/Post/PostContainer.jsx
@@ -3,8 +3,17 @@ import PostFilterContainer from "./PostFilterContainer";
 import PostItem from "./PostItem";
 
 const PostContainer = ({ posts, likedPosts, setLikedPosts }) => {
+  if (posts.length === 0) {
+    return (
+      <div className="mx-auto my-6 max-w-[600px] rounded-lg bg-secondary p-10 shadow-lg">
+        <h2 className="text-lg font-bold">Featured &amp; Local News &gt;</h2>
+        <hr className="my-4 border-t-2 border-[#185450]" />
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
   const [orderedPosts, setOrderedPosts] = useState(posts);
-
+  console.log(posts);
   return (
     <div className="mx-auto my-6 max-w-[600px] rounded-lg bg-secondary p-10 shadow-lg">
       <h2 className="text-lg font-bold">Featured &amp; Local News &gt;</h2>
