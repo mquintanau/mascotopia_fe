f018779360795cfea3ef913f522579a161232642
Carlos Ramirez <cramirezmun@unal.edu.co>
2024-06-02 17:48:28 -0500

    Refactoriza componentes de posts

--- server/client/src/components/Post/PostContainer.jsx
+++ server/client/src/components/Post/PostContainer.jsx
@@ -1,13 +1,60 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import PostFilterContainer from "./PostFilterContainer";
 import PostItem from "./PostItem";
+import Swal from "sweetalert2";
+import { API_URL } from "../../auth/constants";
 
-const PostContainer = ({ posts, likedPosts, setLikedPosts, loadPosts }) => {
+const PostContainer = () => {
+  const [likedPosts, setLikedPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
   const [orderedPosts, setOrderedPosts] = useState(posts);
+  const [filter, setFilter] = useState("All");
+
+  const loadPosts = useCallback(async () => {
+    try {
+      const response = await fetch(`${API_URL}/post/getPosts/`);
+      const data = await response.json();
+      setPosts(data.posts);
+    } catch (error) {
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: { error },
+      });
+    }
+  }, []);
 
   useEffect(() => {
-    setOrderedPosts(posts);
-  }, [posts]);
+    loadPosts();
+  }, [loadPosts]);
+
+  useEffect(() => {
+    if (filter === "All") {
+      setOrderedPosts(posts);
+    } else if (filter === "Local") {
+      const localPosts = [...posts].filter((post) => post.tipo === "local");
+      setOrderedPosts(localPosts);
+    } else if (filter === "Featured") {
+      const featuredPosts = [...posts].filter(
+        (post) => post.tipo === "featured",
+      );
+      setOrderedPosts(featuredPosts);
+    } else if (filter === "Recent") {
+      // make a copy of posts
+      let recentPosts = [...posts];
+
+      recentPosts = recentPosts.sort((a, b) => {
+        const dateA = new Date(a.date);
+        const dateB = new Date(b.date);
+        return dateB - dateA;
+      });
+
+      setOrderedPosts(recentPosts);
+    } else if (filter === "Popular") {
+      const popularPosts = [...posts].sort((a, b) => b.numLikes - a.numLikes);
+      setOrderedPosts(popularPosts);
+    }
+  }, [posts, filter]);
 
   if (posts.length === 0) {
     return (
@@ -23,14 +70,13 @@ const PostContainer = ({ posts, likedPosts, setLikedPosts, loadPosts }) => {
     <div className="mx-auto my-6 max-w-[600px] rounded-lg bg-secondary p-10 shadow-lg">
       <h2 className="text-lg font-bold">Featured &amp; Local News &gt;</h2>
       <hr className="my-4 border-t-2 border-[#185450]" />
-      <PostFilterContainer posts={posts} setOrderedPosts={setOrderedPosts} />
-      {orderedPosts.map((post) => {
+      <PostFilterContainer filter={filter} setFilter={setFilter} />
+      {orderedPosts.map((post, index) => {
         const isLiked = likedPosts.includes(post._id);
         return (
           <PostItem
             value={post}
             key={post._id}
-            setLikedPosts={setLikedPosts}
             isLiked={isLiked}
             loadPosts={loadPosts}
           />
--- server/client/src/components/Post/PostFilter.jsx
+++ server/client/src/components/Post/PostFilter.jsx
@@ -1,59 +1,33 @@
 import { twMerge } from "tailwind-merge";
 
-const PostFilter = ({ name, filter, setFilter, posts, setOrderedPosts }) => {
-  const filterPosts = (filter) => {
-    if (filter === "All") {
-      setOrderedPosts(posts);
-    } else if (filter === "Local") {
-      const localPosts = [...posts].filter((post) => post.tipo === "local");
-      setOrderedPosts(localPosts);
-    } else if (filter === "Featured") {
-      const featuredPosts = [...posts].filter(
-        (post) => post.tipo === "featured",
-      );
-      setOrderedPosts(featuredPosts);
-    } else if (filter === "Recent") {
-      const recentPosts = [...posts].sort(
-        (a, b) => new Date(b.date) - new Date(a.date),
-      );
-      setOrderedPosts(recentPosts);
-    } else if (filter === "Popular") {
-      const popularPosts = [...posts].sort((a, b) => b.numLikes - a.numLikes);
-      setOrderedPosts(popularPosts);
-      console.log(popularPosts);
-    }
-  };
+const PostFilter = ({ name = "", setFilter, active = false }) => {
+  let outputName = "";
 
-  const outputName = (name) => {
-    if (name === "All") {
-      return "" + name;
-    } else if (name === "Local") {
-      return "📍 " + name;
-    } else if (name === "Featured") {
-      return "🌟 " + name;
-    } else if (name === "Recent") {
-      return "🕒 " + name;
-    } else if (name === "Popular") {
-      return "❤️ " + name;
-    }
-  };
+  if (name === "All") {
+    outputName = "" + name;
+  } else if (name === "Local") {
+    outputName = "📍 " + name;
+  } else if (name === "Featured") {
+    outputName = "🌟 " + name;
+  } else if (name === "Recent") {
+    outputName = "🕒 " + name;
+  } else if (name === "Popular") {
+    outputName = "❤️ " + name;
+  }
 
   const filterClass = twMerge(
     "mx-1 my-1 flex h-[30px] w-[110px] items-center justify-center rounded-3xl bg-white text-center hover:cursor-pointer shadow-lg",
-    filter === name
-      ? "font-bold bg-neutral-100 outline outline-2"
-      : "hover:font-bold",
+    active ? "font-bold bg-neutral-100 outline outline-2" : "hover:font-bold",
   );
 
   return (
     <div
       onClick={() => {
-        filterPosts(name);
         setFilter(name);
       }}
       className={filterClass}
     >
-      <p className="">{outputName(name)}</p>
+      <p className="">{outputName}</p>
     </div>
   );
 };
