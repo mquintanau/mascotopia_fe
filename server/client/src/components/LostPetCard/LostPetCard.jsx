3a6e0d8d0bc8431a820c5f1a9199f97372907539
mquintanau <mquintanau@unal.edu.co>
2024-06-04 18:24:42 -0500

    Correccion imagenes

--- server/client/src/components/LostPetCard/LostPetCard.jsx
+++ server/client/src/components/LostPetCard/LostPetCard.jsx
@@ -13,7 +13,7 @@ const LostPetCard = ({ pet, className }) => {
         <h3 className="w-full break-words text-xl font-bold">{nombre}</h3>
         <img
           className="rounded-lg"
-          src={`https://mascotopia-fe.onrender.com/:4000${imageURL}`}
+          src={`https://mascotopia-fe.onrender.com${imageURL}`}
           alt="lost pet"
         />
       </div>
